ac61a5b4b6e775c8bb7214f3de2a1776b0ac63b8
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-05-23 16:02:16 -0300

    deploy ok

--- app/csr/page.tsx
+++ app/csr/page.tsx
@@ -6,18 +6,21 @@ import { useEffect, useState } from 'react';
 export default function CSR() {
   const [todos, setTodos] = useState([]);
 
+  const fetchTodos = async () => {
+    const response = await fetch('/api/todos');
+    const data = await response.json();
+    setTodos(data);
+  };
+
   useEffect(() => {
-    const fetchTodos = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const data = await res.json();
-      setTodos(data);
-    };
     fetchTodos();
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-xl text-red-500">Client side rendered</h1>
+      <h1 className="text-xl text-red-500" onClick={fetchTodos}>
+        Client side rendered
+      </h1>
       {todos.map((todo: { id: string; title: string }) => (
         <p key={todo.id}>{todo.title}</p>
       ))}
